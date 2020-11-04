0abaaab9976ea10287150f756697b1766951d036
Johan Gustafsson <jhngstfssn@gmail.com>
2020-11-04 14:25:42 +0100

    Rewrite Boltzmann machine

--- Homework 3/Part 3/restricted_boltzmann_machine.py
+++ Homework 3/Part 3/restricted_boltzmann_machine.py
@@ -1,103 +1,89 @@
 import numpy as np
 
-PATTERNS = np.matrix([[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                      [+1, -1, -1, +1, -1, -1, +1, -1, -1],
-                      [-1, +1, -1, -1, +1, -1, -1, +1, -1],
-                      [-1, -1, +1, -1, -1, +1, -1, -1, +1],
-                      [+1, +1, -1, +1, +1, -1, +1, +1, -1],
-                      [-1, +1, +1, -1, +1, +1, -1, +1, +1],
-                      [+1, -1, +1, +1, -1, +1, +1, -1, +1],
-                      [+1, +1, +1, +1, +1, +1, +1, +1, +1],
-                      [+1, +1, +1, -1, -1, -1, -1, -1, -1],
-                      [-1, -1, -1, +1, +1, +1, -1, -1, -1],
-                      [-1, -1, -1, -1, -1, -1, +1, +1, +1],
-                      [+1, +1, +1, +1, +1, +1, -1, -1, -1],
-                      [-1, -1, -1, +1, +1, +1, +1, +1, +1],
-                      [+1, +1, +1, -1, -1, -1, +1, +1, +1]])
+# Patterns
+PATTERNS = np.array([[-1, -1, -1, -1, -1, -1, -1, -1, -1],
+                     [+1, -1, -1, +1, -1, -1, +1, -1, -1],
+                     [-1, +1, -1, -1, +1, -1, -1, +1, -1],
+                     [-1, -1, +1, -1, -1, +1, -1, -1, +1],
+                     [+1, +1, -1, +1, +1, -1, +1, +1, -1],
+                     [-1, +1, +1, -1, +1, +1, -1, +1, +1],
+                     [+1, -1, +1, +1, -1, +1, +1, -1, +1],
+                     [+1, +1, +1, +1, +1, +1, +1, +1, +1],
+                     [+1, +1, +1, -1, -1, -1, -1, -1, -1],
+                     [-1, -1, -1, +1, +1, +1, -1, -1, -1],
+                     [-1, -1, -1, -1, -1, -1, +1, +1, +1],
+                     [+1, +1, +1, +1, +1, +1, -1, -1, -1],
+                     [-1, -1, -1, +1, +1, +1, +1, +1, +1],
+                     [+1, +1, +1, -1, -1, -1, +1, +1, +1]])
 
+# Network parameters
 N_PATTERNS = 14
 N_VISIBLE = 9
-N_HIDDEN = 2
+N_HIDDEN = 8
 
-N_EPOCHS = 1000
+# Training parameters
+N_EPOCHS = 50
 N_SAMPLES = 10000
 N_TIMES = 100
 N_TRANSIENT = 20
 
 
 class Boltzmann:
-    def __init__(self, n_visible, n_hidden):
+    def __init__(self, n_visible=N_VISIBLE, n_hidden=N_HIDDEN):
         self.n_visible = n_visible
         self.n_hidden = n_hidden
-        self.weights = self.get_weights()
-        self.v_bias = self.get_thresholds(True)
-        self.h_bias = self.get_thresholds(False)
-        self.learning_rate = 0.1
+        self.weights = self.get_weights(n_hidden, n_visible)
+        self.v_bias = self.get_thresholds(n_visible)
+        self.h_bias = self.get_thresholds(n_hidden)
+        self.learning_rate = 0.01
 
-    def get_weights(self, mu = 0, sigma = 0.01):
-        normal = np.random.randn(self.n_visible, self.n_hidden)
-        return mu + sigma * normal
+    # ------------------------
+    # Initialization functions
+    # ------------------------
+    def get_weights(self, x, y):
+        return 0.01 * np.random.randn(x, y)
 
-    def get_thresholds(self, visible = True):
-        if visible:
-            # TODO Do the fancy initialization
-            return np.zeros(self.n_visible)
-        return np.zeros(self.n_hidden)
+    def get_thresholds(self, x):
+        return np.zeros(x)
 
-    def get_pattern(self, size):
+    # -----------------
+    # Utility functions
+    # -----------------
+    def get_random_pattern(self, size=N_VISIBLE):
         return 2 * np.random.randint(2, size=size) - 1
 
-    def get_pattern_index(self, output):
-        for counter, pattern in enumerate(PATTERNS):
+    def get_index(self, output, patterns=PATTERNS):
+        for index, pattern in enumerate(patterns):
             if (pattern == output).all():
-                return counter
-        return None
+                return index
 
-    def sigmoid(self, b):
-        return 1 / (1 + np.exp(-2 * b))
+    # -------------------
+    # Boltzmann functions
+    # -------------------
+    def train(self, patterns=PATTERNS, n_patterns=N_PATTERNS, n_epochs=N_EPOCHS, n_times=N_TIMES):
+        divergences = np.zeros(n_epochs)
 
-    def stochastic_update(self, local_field):
-        p = self.sigmoid(local_field)
-        r = np.random.rand()
-        output = 1 if r < p else -1
-        return output
+        for index in range(n_epochs):
 
-    def kullback_leibler(self, pbs):
-        result = 0
-        p_data = 1 / N_PATTERNS
-        for pb in pbs:
-            temp = p_data * np.log(p_data / pb)
-            result += temp
-        return result
-
-
-    def train(self):
-        for _ in range(N_EPOCHS):
             # Choose random pattern
-            p0 = PATTERNS[np.random.randint(N_PATTERNS)]
+            pattern_index = np.random.randint(n_patterns)
+            pattern = patterns[pattern_index]
 
-            # Set visible neurons
-            v0 = p0
-            bh0 = v0 * self.weights - self.h_bias
+            # Initialize neurons
+            v0, h0 = pattern, self.update_hidden(pattern)
 
-            # Update hidden neurons
-            h0 = np.zeros((1, 2))
-            for index in range(self.n_hidden):
-                h0[0, index] = self.stochastic_update(bh0[0, index])
+            # Iterate for contrastive divergence
+            vk, hk = v0, h0
+            for _ in range(n_times):
+                vk = self.update_visible(hk)
+                hk = self.update_hidden(vk)
 
-            # Iterate
-            vk = v0
-            hk = h0
-            for _ in range(N_TIMES):
-                bvk = self.weights @ np.transpose(hk) - self.v_bias
-                for index in range(self.n_visible):
-                    vk[0, index] = self.stochastic_update(bvk[0, index])
-                bhk = vk * self.weights - self.h_bias
-                for index in range(self.n_hidden):
-                    hk[0, index] = self.stochastic_update(bhk[0, index])
+            # Calculate local fields
+            bh0 = self.weights @ v0 - self.h_bias
+            bhk = self.weights @ vk - self.h_bias
 
             # Calculate changes
-            dw = np.outer(v0, np.tanh(bh0)) - np.outer(vk, np.tanh(bhk))
+            dw = np.outer(np.tanh(bh0), v0) - np.outer(np.tanh(bhk), vk)
             dtv = v0 - vk
             dth = np.tanh(bh0) - np.tanh(bhk)
 
@@ -107,45 +93,73 @@ class Boltzmann:
             self.h_bias = self.h_bias - dth * self.learning_rate
 
             # Kullback-Leibler divergence
-            pb = self.approximate_distribution()
-            kld = self.kullback_leibler(pb)
-            print(kld)
+            distribution = self.approximate_distribution()
+            divergence = self.kullback_leibler(distribution)
+            divergences[index] = divergence
 
-    def run(self, data):
-        v0 = np.reshape(data, (1, self.n_visible))
-        bh0 = v0 @ self.weights - self.h_bias
-        
-        # Update hidden neurons
-        h0 = np.zeros((1, 2))
-        for index in range(self.n_hidden):
-            h0[0, index] = self.stochastic_update(bh0[0, index])
+        return divergences
 
-        # Iterate
-        vk = v0
-        hk = h0
-        for _ in range(N_TRANSIENT):
-            bvk = self.weights @ np.transpose(hk) - self.v_bias
-            for index in range(self.n_visible):
-                vk[0, index] = self.stochastic_update(bvk[0, index])
-            bhk = vk @ self.weights - self.h_bias
-            for index in range(self.n_hidden):
-                hk[0, index] = self.stochastic_update(bhk[0, index])
+    def run(self, pattern, n_transient=N_TRANSIENT):
+
+        # Initialize neurons
+        v0, h0 = pattern, self.update_hidden(pattern)
+
+        # Iterate beyond transient
+        vk, hk = v0, h0
+        for _ in range(n_transient):
+            vk = self.update_visible(hk)
+            hk = self.update_hidden(vk)
+
         return vk
 
-    def approximate_distribution(self):
-        outputs = np.zeros(N_PATTERNS)
-        for _ in range(N_SAMPLES):
-            pattern = self.get_pattern(self.n_visible)
+    def update_hidden(self, visible):
+        hidden = np.zeros(self.n_hidden)
+        for h in range(self.n_hidden):
+            w = self.weights[h, :]
+            b = np.dot(w, visible) - self.h_bias[h]
+            hidden[h] = self.stochastic_update(b)
+        return hidden
+
+    def update_visible(self, hidden):
+        visible = np.zeros(self.n_visible)
+        for v in range(self.n_visible):
+            w = self.weights[:, v]
+            b = np.dot(w, hidden) - self.v_bias[v]
+            visible[v] = self.stochastic_update(b)
+        return visible
+
+    def stochastic_update(self, local_field):
+        p = self.sigmoid(local_field)
+        r = np.random.rand()
+        output = 1 if r < p else -1
+        return output
+
+    def sigmoid(self, b):
+        return 1 / (1 + np.exp(-2 * b))
+
+    def kullback_leibler(self, distribution, n_patterns=N_PATTERNS):
+        result = 0
+        p_data = 1 / n_patterns
+        for pb in distribution:
+            temp = p_data * np.log(p_data / pb)
+            result += temp
+        return result
+
+    def approximate_distribution(self, n_patterns=N_PATTERNS, n_samples=N_SAMPLES):
+        outputs = np.zeros(n_patterns)
+        for _ in range(n_samples):
+            pattern = self.get_random_pattern()
             output = self.run(pattern)
-            index = self.get_pattern_index(output)
+            index = self.get_index(output)
             if index is not None:
                 outputs[index] += 1
-        return outputs / np.sum(outputs)
+        return outputs / n_samples
 
 
 def main():
-    boltzmann = Boltzmann(N_VISIBLE, N_HIDDEN)
-    boltzmann.train()
+    boltzmann = Boltzmann()
+    divergences = boltzmann.train()
+    # plot divergences here
 
 
 if __name__ == '__main__':
